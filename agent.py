a43424c31b99ca550806ca732d54b8bf0a0846d9
Bui Minh Nhan <nhan.bui@NakivoVN.local>
2023-01-12 21:13:27 +0700

    update

--- agent.py
+++ agent.py
@@ -1,40 +1,25 @@
+
 from flask import Flask, render_template, request, jsonify
-
 import webbrowser
-
 import subprocess
-
 import requests
-
-
 
 app = Flask(__name__, template_folder='D:/Python')
 coccoc_location = r"C:\Program Files (x86)\CocCoc\Browser\Application\browser.exe"
 
 
-
 @app.route('/')
 def index():
-
     return render_template('index.html')
 
 
-
 @app.route('/download', methods=['POST'])
-
 def download():
-
     input_command = request.json.get('input_command')
-
     subprocess.Popen(f'"{coccoc_location}" --start-minimized {input_command}',
-
                      creationflags=subprocess.CREATE_NO_WINDOW)
-
     return jsonify({"message": "Browser opened with the link"}), 200
 
 
-
-if _name_ == '__main__':
-
-app.run(host='0.0.0.0')
-
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
